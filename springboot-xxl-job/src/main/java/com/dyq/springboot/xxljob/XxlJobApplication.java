9ae63ae2ac1aaf1ba67751b636fdf9859b3e13f4
zhanguomozhu <zhanguomozhu@users.noreply.github.com>
2023-04-26 16:32:44 +0800

    springboot xxl-job 练习

---
+++ springboot-xxl-job/src/main/java/com/dyq/springboot/xxljob/XxlJobApplication.java
@@ -0,0 +1,13 @@
+package com.dyq.springboot.xxljob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobApplication.class, args);
+    }
+
+}
